c9305bfe832d042930bec22156b991b1f918a613
m <20924663+migbash@users.noreply.github.com>
2021-07-16 23:26:59 +0100

    updating the `comments` for the `gentType_data.js` file

--- src/store/agentType_data.js
+++ src/store/agentType_data.js
@@ -1,3 +1,12 @@
+/**
+ * Stores the different types of 
+ * agents in the website project,
+ * ====
+ * Currently only limited to 2 
+ * different agents, and up-to MAX 4
+ * different agents can exists in this file
+ * that will not distort the designs,
+*/
 export const agentType_data = [
     {
         'name': `Social Agent`,
